fbb9a3c8ade3d62370f5dfa174dec8380f92b4ca
1ranxu <263989312@qq.com>
2023-11-17 22:41:30 +0800

    给业务服务项目启动类打上注解，开启服务发现、找到对应的客户端 Bean 的位置

--- luooj-backend-judge-service/src/main/java/com/luoying/luoojbackendjudgeservice/LuoojBackendJudgeServiceApplication.java
+++ luooj-backend-judge-service/src/main/java/com/luoying/luoojbackendjudgeservice/LuoojBackendJudgeServiceApplication.java
@@ -2,6 +2,8 @@ package com.luoying.luoojbackendjudgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +12,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @ComponentScan("com.luoying")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.luoying.luoojbackendserviceclient.service"})
 public class LuoojBackendJudgeServiceApplication {
 
     public static void main(String[] args) {
