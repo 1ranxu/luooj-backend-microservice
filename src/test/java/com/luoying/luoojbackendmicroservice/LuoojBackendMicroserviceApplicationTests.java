55c3d54ba01088ae9dfd1609dab1cdd81be94ce3
1ranxu <263989312@qq.com>
2023-11-17 20:23:05 +0800

    luooj-backend-microservice项目初始化

---
+++ src/test/java/com/luoying/luoojbackendmicroservice/LuoojBackendMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luoying.luoojbackendmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuoojBackendMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
