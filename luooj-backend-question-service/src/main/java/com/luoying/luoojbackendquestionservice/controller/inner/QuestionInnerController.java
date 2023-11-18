b6be1e942d19ed4842387bff8072c76553e4cd08
1ranxu <263989312@qq.com>
2023-11-18 13:33:46 +0800

    QuestionInnerController补充RestController和RequestMapping注解

--- luooj-backend-question-service/src/main/java/com/luoying/luoojbackendquestionservice/controller/inner/QuestionInnerController.java
+++ luooj-backend-question-service/src/main/java/com/luoying/luoojbackendquestionservice/controller/inner/QuestionInnerController.java
@@ -5,12 +5,18 @@ import com.luoying.luoojbackendmodel.entity.QuestionSubmit;
 import com.luoying.luoojbackendserviceclient.service.QuestionFeignClient;
 import com.luoying.luoojbackendquestionservice.service.QuestionService;
 import com.luoying.luoojbackendquestionservice.service.QuestionSubmitService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 /**
  * 该服务仅内部调用，不是给前端的
  */
+@RestController
+@RequestMapping("/inner")
 public class QuestionInnerController implements QuestionFeignClient {
 
     @Resource
@@ -20,16 +26,19 @@ public class QuestionInnerController implements QuestionFeignClient {
     private QuestionSubmitService questionSubmitService;
 
     @Override
+    @GetMapping("/get/id")
     public Question getQuestionById(long questionId) {
         return questionService.getById(questionId);
     }
 
     @Override
+    @GetMapping("/question_submit/get/id")
     public QuestionSubmit getQuestionSubmitById(long questionSubmitId) {
         return questionSubmitService.getById(questionSubmitId);
     }
 
     @Override
+    @PostMapping("/question_submit/update")
     public Boolean updateQuestionSubmitById(QuestionSubmit questionSubmit) {
         return questionSubmitService.updateById(questionSubmit);
     }
