77ee7e830509eba9031a5dd1f3027db2c3646cf4
1ranxu <263989312@qq.com>
2023-11-18 12:30:31 +0800

    消息队列解耦：消费者代码-判题服务

---
+++ luooj-backend-question-service/src/main/java/com/luoying/luoojbackendquestionservice/rabbitmq/MessageProducer.java
@@ -0,0 +1,22 @@
+package com.luoying.luoojbackendquestionservice.rabbitmq;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class MessageProducer {
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * 发送消息
+     * @param exchange
+     * @param routingKey
+     * @param message
+     */
+    public void sendMessage(String exchange, String routingKey, String message) {
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
