6a1c4b19c1f35378d72655bfc364081bc26ef057
1ranxu <263989312@qq.com>
2024-10-12 17:50:53 +0800

    update：更新线程池配置

--- luooj-backend-common/src/main/java/com/luoying/luoojbackendcommon/config/ThreadPoolExecutorConfig.java
+++ luooj-backend-common/src/main/java/com/luoying/luoojbackendcommon/config/ThreadPoolExecutorConfig.java
@@ -1,32 +1,39 @@
 package com.luoying.luoojbackendcommon.config;
 
-import org.jetbrains.annotations.NotNull;
+import com.sun.istack.internal.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 @Configuration
 public class ThreadPoolExecutorConfig {
+    private static final int coreThreads = 2 * Runtime.getRuntime().availableProcessors();
+    private static final int maxThreads = 2 * coreThreads;
+    private static final long keepAliveTime = 0L;
+    private static final TimeUnit unit = TimeUnit.SECONDS;
+    private static final LinkedBlockingQueue queue = new LinkedBlockingQueue(1000);
+
+    private static final String threadName = "pool-thread-";
+
+    private static final ThreadFactory threadFactory = new ThreadFactory() {
+        private int count = 1;
+
+        @Override
+        public Thread newThread(@NotNull Runnable r) {
+            Thread thread = new Thread(r);
+            thread.setName(threadName + count);
+            count++;
+            return thread;
+        }
+    };
+
+    private static final RejectedExecutionHandler policy = new ThreadPoolExecutor.CallerRunsPolicy();
 
 
     @Bean
     public ThreadPoolExecutor threadPoolExecutor() {
-        ThreadFactory threadFactory = new ThreadFactory() {
-            private int count = 1;
-
-            @Override
-            public Thread newThread(@NotNull Runnable r) {
-                Thread thread = new Thread(r);
-                thread.setName("线程" + count);
-                count++;
-                return thread;
-            }
-        };
-        return new ThreadPoolExecutor(8, 16, 100, TimeUnit.SECONDS,
-                new ArrayBlockingQueue<>(4), threadFactory, new ThreadPoolExecutor.AbortPolicy());
+        return new ThreadPoolExecutor(coreThreads, maxThreads, keepAliveTime, unit,
+                queue, threadFactory, policy);
     }
 }
